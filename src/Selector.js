ae99a2cccad81973114f2365feec7b7728981947
Maciek Chmura <chmura.m@gmail.com>
2018-10-15 09:07:22 +0200

    selector component with state

--- src/Selector.js
+++ src/Selector.js
@@ -3,50 +3,33 @@ import React from "react";
 class Selector extends React.Component {
   constructor(props) {
     super(props);
+    this.inputs = [0, 1, 2, 3, 4];
+    this.state = {
+      checked: 2
+    };
   }
+  handleClick = event => {
+    this.setState({ checked: +event.target.value });
+  };
 
   render() {
     return (
       <form className="selector">
+        <p>{this.state.checked}</p>
         <div className="container">
           <div className="child label">Quality of work</div>
-          <div className="child button-parent">
-            <input
-              className="radio"
-              type="radio"
-              id="huey"
-              name="drone"
-              value="0"
-            />
-            <input
-              className="radio"
-              type="radio"
-              id="huey"
-              name="drone"
-              value="1"
-            />
-            <input
-              className="radio"
-              type="radio"
-              id="huey"
-              name="drone"
-              value="2"
-              checked
-            />
-            <input
-              className="radio"
-              type="radio"
-              id="huey"
-              name="drone"
-              value="3"
-            />
-            <input
-              className="radio"
-              type="radio"
-              id="huey"
-              name="drone"
-              value="4"
-            />
+          <div className="child button-parent" onChange={this.handleClick}>
+            {this.inputs.map(value => (
+              <input
+                key={value}
+                className="radio"
+                type="radio"
+                id="huey"
+                name="drone"
+                value={value}
+                defaultChecked={this.state.checked === value}
+              />
+            ))}
           </div>
         </div>
       </form>
